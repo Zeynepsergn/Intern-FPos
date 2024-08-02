44b67a034264fc01838293c670cc13bcadd23ce2
gokcenes <213301094@ogr.selcuk.edu.tr>
2024-08-02 17:37:21 +0300

    Proje oluşturuldu.

---
+++ src/main/java/tr/gov/gib/fpos/FPosApplication.java
@@ -0,0 +1,13 @@
+package tr.gov.gib.fpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FPosApplication.class, args);
+	}
+
+}
